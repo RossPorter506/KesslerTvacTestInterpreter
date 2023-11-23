ceb7d808d1f89ca503ad13485e795ea17565cc43
Ross Porter <rossporter506@gmail.com>
2023-11-23 17:22:24 +1300

    Somewhat working
    Just need to deal with output csv file now.

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@
 use anyhow::{Result, anyhow, bail};
 use std::fs::File;
 use std::io::{BufReader, BufRead};
+use csv::WriterBuilder;
 
-const EMISSION_LINES: usize = 11;
+const SELF_TEST_LINES: usize= 221;
+const EMISSION_LINES: usize = 33;
 const DEPLOYMENT_LINES: usize = 13;
-const PAYLOAD_OFF_LINES: usize = 33;
+const PAYLOAD_OFF_LINES: usize = 11;
 
 const TVAC_EMISSION: Tvac = Tvac{packet_size: EMISSION_LINES, state: TvacState::Emission};
 const TVAC_DEPLOYMENT: Tvac = Tvac{packet_size: DEPLOYMENT_LINES, state: TvacState::Deployment};
@@ -14,44 +16,68 @@ const TVAC_PAYLOAD_OFF: Tvac = Tvac{packet_size: PAYLOAD_OFF_LINES, state: TvacS
 
 fn main() -> Result<()>{
 	// Open text file
-	let f = File::open("TVACTest.txt")?;
+	let f = File::open("TVACTest-original.txt")?;
 	let mut lines = BufReader::new(f).lines().flatten().peekable();
 
 	let mut v: Vec<SensorData> = vec![];
 
 	// Self-test at the beginning. Ignore for now.
-	let r = lines.next_chunk::<220>();
+	let r = lines.next_chunk::<SELF_TEST_LINES>();
 	if r.is_err() { bail!("Failed to read initial self-test");}
 
 	let mut state = TVAC_PAYLOAD_OFF;
 
-	loop {
+	while let Some(next_line) = lines.peek() {
+		if next_line.is_empty() {
+			lines.next();
+			continue;
+		}
+
+		// See if the next line announces a change of state. If so, update our state.
+		if let Some(new_state) = state_change(next_line) {
+			state = new_state;
+			lines.next();
+		}
+
 		// Get all the lines we need to parse one packet
 		let chunk = match chunk_of(state.packet_size, &mut lines) {
 			Ok(lns) => lns,
-			Err(e) => {eprintln!("Failed to acquire chunk: {e}"); break},
+			Err(e) => {eprintln!("{e}"); break},
 		};
 
-		// Interpret strings
-		let packet = match state.interpret_packet(chunk) {
+		// Interpret chunk
+		let packet = match state.interpret_packet(chunk.clone()) {
 			Ok(data) => data,
-			Err(e) => {eprintln!("Failed to parse chunk: {e}"); break},
+			Err(e) => {
+				eprintln!("Failed to parse chunk: {e}. Chunk written to failed_chunks."); 
+				write_broken_chunk_to_file(chunk); 
+				let mut line = lines.next();
+				while line.is_some() && !(line.unwrap()).is_empty() {
+					line = lines.next();
+				}
+				continue},
 		};
 
 		// Save results to a vec for now
 		v.push(packet);
-
-		// Peek next line and see if it's a change of state. If so, update our state.
-		let next_line = lines.peek();
-
-		match next_line {
-			Some(line) => {if let Some(new_state) = state_change(line) {state = new_state}},
-			None => break, // No next line to get - end of file.
-		}
 	}
 	// Do something with v
-	println!("{v:?}");
-	todo!()
+	//println!("{v:?}");
+	let mut wtr = WriterBuilder::new().has_headers(true).flexible(true).from_path("out.csv")?;
+
+	for record in v {
+		wtr.serialize(record)?;
+	}
+
+	println!("Written to out.csv");
+	Ok(())
+}
+
+fn write_broken_chunk_to_file(chunk: Vec<String>) {
+	static mut FILE_NUM: u32 = 0;
+	let file_path = unsafe{ format!("failed_chunks/chunk{FILE_NUM}.txt") };
+	unsafe{FILE_NUM += 1};
+	std::fs::write(file_path, chunk.join("\n")).unwrap();
 }
 
 /// Returns a vector of n elements, or an error if the iterator returns none.
@@ -75,22 +101,8 @@ fn state_change(str: &str) -> Option<Tvac> {
 fn interpret_payload_off_packet(arr: [String;11]) -> Result<SensorData> {
 	Ok(
 		SensorData::PayloadOff(
-			PayloadOffSensors{
-				time: Time {
-					phase: extract_nth_word_as_number(&arr[0], 0)?,
-					total: extract_nth_word_as_number(&arr[1], 0)?,
-				},
-				temp: Temperatures {
-					lms_emit: extract_nth_word_as_number(&arr[2], 1)?,
-					lms_rec: extract_nth_word_as_number(&arr[3], 1)?,
-					msp: extract_nth_word_as_number(&arr[4], 1)?,
-					heater: extract_nth_word_as_number(&arr[5], 1)?,
-					hvdc: extract_nth_word_as_number(&arr[6], 1)?,
-					tether_monitoring: extract_nth_word_as_number(&arr[7], 1)?,
-					tether_connector: extract_nth_word_as_number(&arr[8], 1)?,
-					msp_3v3_supply: extract_nth_word_as_number(&arr[9], 1)?,
-				},
-			}
+			extract_time(&arr[0..2])?,
+			extract_temperatures(&arr[2..10])?,
 		)
 	)
 }
@@ -98,26 +110,12 @@ fn interpret_payload_off_packet(arr: [String;11]) -> Result<SensorData> {
 fn interpret_deployment_packet(arr: [String;13]) -> Result<SensorData> {
 	Ok(
 		SensorData::Deployment(
-			DeploymentSensors{
-				time: Time {
-					phase: extract_nth_word_as_number(&arr[0], 0)?,
-					total: extract_nth_word_as_number(&arr[1],0)?,
-				},
-				pinpuller: Pinpuller {
-					current: extract_measurement_from_nth_word(&arr[2], 3, "mA")?,
-					acc: extract_measurement_from_nth_word(&arr[3], 3, "%")?
-				},
-				temp: Temperatures {
-					lms_emit: extract_nth_word_as_number(&arr[4], 1)?,
-					lms_rec: extract_nth_word_as_number(&arr[5], 1)?,
-					msp: extract_nth_word_as_number(&arr[6], 1)?,
-					heater: extract_nth_word_as_number(&arr[7], 1)?,
-					hvdc: extract_nth_word_as_number(&arr[8], 1)?,
-					tether_monitoring: extract_nth_word_as_number(&arr[9], 1)?,
-					tether_connector: extract_nth_word_as_number(&arr[10], 1)?,
-					msp_3v3_supply: extract_nth_word_as_number(&arr[11], 1)?,
-				},
-			}
+			extract_time(&arr[0..2])?,
+			extract_temperatures(&arr[4..12])?,
+			Pinpuller {
+				current: extract_measurement_from_nth_word(&arr[2], 3, "mA")?,
+				acc: extract_measurement_from_nth_word(&arr[3], 3, "%")?
+			},
 		)
 	)
 }
@@ -125,49 +123,56 @@ fn interpret_deployment_packet(arr: [String;13]) -> Result<SensorData> {
 fn interpret_emission_packet(arr: [String; 33]) -> Result<SensorData> {
 	Ok(
 		SensorData::Emission(
-			EmissionSensors {
-				time: Time {
-					phase: extract_nth_word_as_number(&arr[0], 0)?,
-					total: extract_nth_word_as_number(&arr[1], 0)?,
+			extract_time(&arr[0..2])?,
+			extract_temperatures(&arr[25..33])?,
+			TetherSensors {
+				cathode_offset: CathodeOffsetSupply {
+					voltage: extract_measurement_from_nth_word(&arr[2], 3, "mV")?,
+					current: extract_measurement_from_nth_word(&arr[3], 3, "uA")?,
+					v_acc: extract_measurement_from_nth_word(&arr[7], 3, "%")?,
+					c_acc: extract_measurement_from_nth_word(&arr[8], 3, "%")?,
 				},
-				emitter: TetherSensors {
-					cathode_offset: CathodeOffsetSupply {
-						voltage: extract_measurement_from_nth_word(&arr[2], 3, "mV")?,
-						current: extract_measurement_from_nth_word(&arr[3], 3, "uA")?,
-						v_acc: extract_measurement_from_nth_word(&arr[7], 3, "%")?,
-						c_acc: extract_measurement_from_nth_word(&arr[8], 3, "%")?,
-					},
-					tether_bias: TetherBiasSupply {
-						voltage: extract_measurement_from_nth_word(&arr[9], 3, "mV")?,
-						current: extract_measurement_from_nth_word(&arr[10], 3, "uA")?,
-						v_acc: extract_measurement_from_nth_word(&arr[14],3, "%")?,
-						c_acc: extract_measurement_from_nth_word(&arr[15], 3, "%")?,
-					},
-					heater: HeaterSupply {
-						voltage: extract_measurement_from_nth_word(&arr[16], 3, "mV")?,
-						current: extract_measurement_from_nth_word(&arr[17], 3, "mA")?,
-						v_acc: extract_measurement_from_nth_word(&arr[20], 2, "%")?,
-						c_acc: extract_measurement_from_nth_word(&arr[21], 2, "%")?
-					},
-					repeller: Repeller {
-						voltage: extract_measurement_from_nth_word(&arr[22], 3, "mV")?,
-						v_acc: extract_measurement_from_nth_word(&arr[24], 2, "%")?
-					},
+				tether_bias: TetherBiasSupply {
+					voltage: extract_measurement_from_nth_word(&arr[9], 3, "mV")?,
+					current: extract_measurement_from_nth_word(&arr[10], 3, "uA")?,
+					v_acc: extract_measurement_from_nth_word(&arr[14],3, "%")?,
+					c_acc: extract_measurement_from_nth_word(&arr[15], 3, "%")?,
 				},
-				temp: Temperatures {
-					lms_emit: extract_nth_word_as_number(&arr[2], 1)?,
-					lms_rec: extract_nth_word_as_number(&arr[3], 1)?,
-					msp: extract_nth_word_as_number(&arr[4], 1)?,
-					heater: extract_nth_word_as_number(&arr[5], 1)?,
-					hvdc: extract_nth_word_as_number(&arr[6], 1)?,
-					tether_monitoring: extract_nth_word_as_number(&arr[7], 1)?,
-					tether_connector: extract_nth_word_as_number(&arr[8], 1)?,
-					msp_3v3_supply: extract_nth_word_as_number(&arr[9], 1)?,
+				heater: HeaterSupply {
+					voltage: extract_measurement_from_nth_word(&arr[16], 3, "mV")?,
+					current: extract_measurement_from_nth_word(&arr[17], 3, "mA")?,
+					v_acc: extract_measurement_from_nth_word(&arr[20], 2, "%")?,
+					c_acc: extract_measurement_from_nth_word(&arr[21], 2, "%")?
 				},
-			}
+				repeller: Repeller {
+					voltage: extract_measurement_from_nth_word(&arr[22], 3, "mV")?,
+					v_acc: extract_measurement_from_nth_word(&arr[24], 2, "%")?
+				},
+			},
 		)
 	)
 }
+
+fn extract_temperatures(slice: &[String]) -> Result<Temperatures> {
+	Ok(Temperatures {
+		lms_emit: extract_nth_word_as_number(&slice[0], 2)?,
+		lms_rec: extract_nth_word_as_number(&slice[1], 2)?,
+		msp: extract_nth_word_as_number(&slice[2], 1)?,
+		heater: extract_nth_word_as_number(&slice[3], 2)?,
+		hvdc: extract_nth_word_as_number(&slice[4], 2)?,
+		tether_monitoring: extract_nth_word_as_number(&slice[5], 2)?,
+		tether_connector: extract_nth_word_as_number(&slice[6], 2)?,
+		msp_3v3_supply: extract_nth_word_as_number(&slice[7], 3)?,
+	})
+}
+
+fn extract_time(slice: &[String]) -> Result<Time> {
+	Ok(Time {
+		phase_time: extract_nth_word_as_number(&slice[0], 0)?,
+		total_time: extract_nth_word_as_number(&slice[1], 0)?,
+	})
+}
+
 /// Given a string like: `[ OK ] Measured output voltage: 259372mV` or `[FAIL] Measured output voltage: 259372mV`,
 /// returns `259372` when provided appropriate word number and suffix to remove.
 fn extract_measurement_from_nth_word<'a, T>(str: &'a str, n: usize, suffix: &'a str) -> Result<T>
@@ -191,7 +196,7 @@ fn crop_first_n_letters(s: &str, n: usize) -> &str {
 }
 
 fn extract_nth_word(str: &str, n: usize) -> Result<&str> {
-	str.split_ascii_whitespace().nth(n).ok_or(anyhow!("Failed to extract {n}th word from '{str}'"))
+	str.split_ascii_whitespace().nth(n).ok_or(anyhow!("Failed to extract {n}th word from '{str}': `{str}`"))
 }
 
 /// Given a string like `LMS Emitter: 78` returns 78 (as a number).
@@ -199,8 +204,9 @@ fn extract_nth_word_as_number<T>(str: &str, n: usize) -> Result<T>
 where
 	T: std::str::FromStr,
 	anyhow::Error: From<T::Err>{
+	
 	let nth_word = str.split_ascii_whitespace().nth(n).ok_or(anyhow!("Failed to extract {n}th word from '{str}'"))?;
-	nth_word.parse().map_err(|_| anyhow!("Failed to parse '{nth_word}' as number"))
+	nth_word.parse().map_err(|_| anyhow!("Failed to parse '{nth_word}' as number: `{str}`"))
 }
 
 #[derive(Debug)]
@@ -225,58 +231,58 @@ enum TvacState {
 	Emission,
 }
 
-#[derive(Debug)]
+#[derive(Debug, serde::Serialize)]
 enum SensorData {
-	PayloadOff(PayloadOffSensors),
-	Deployment(DeploymentSensors),
-	Emission(EmissionSensors),
+	PayloadOff(Time, Temperatures),
+	Deployment(Time, Temperatures, Pinpuller),
+	Emission(  Time, Temperatures, TetherSensors),
 }
 
-#[derive(Debug)]
+#[derive(Debug, serde::Serialize)]
 struct Time {
-	total: u32,
-	phase: u32,
+	total_time: u32,
+	phase_time: u32,
 }
 
-#[derive(Debug)]
+#[derive(Debug, serde::Serialize)]
 struct PayloadOffSensors{
 	temp: Temperatures,
 	time: Time,
 }
 
-#[derive(Debug)]
+#[derive(Debug, serde::Serialize)]
 struct DeploymentSensors {
 	temp: Temperatures,
 	time: Time,
 	pinpuller: Pinpuller,
 }
 
-#[derive(Debug)]
+#[derive(Debug, serde::Serialize)]
 struct EmissionSensors {
 	temp: Temperatures,
 	time: Time,
 	emitter: TetherSensors,
 }
 
-#[derive(Debug)]
+#[derive(Debug, serde::Serialize)]
 struct Temperatures {
-	lms_emit: u16,
-	lms_rec: u16,
-	msp: u16,
-	heater: u16,
-	hvdc: u16,
-	tether_monitoring: u16,
-	tether_connector: u16,
-	msp_3v3_supply: u16,
+	lms_emit: i16,
+	lms_rec: i16,
+	msp: i16,
+	heater: i16,
+	hvdc: i16,
+	tether_monitoring: i16,
+	tether_connector: i16,
+	msp_3v3_supply: i16,
 }
 
-#[derive(Debug)]
+#[derive(Debug, serde::Serialize)]
 struct Pinpuller {
 	current: u16,
 	acc: f32,
 }
 
-#[derive(Debug)]
+#[derive(Debug, serde::Serialize)]
 struct TetherSensors {
 	cathode_offset: CathodeOffsetSupply,
 	tether_bias: TetherBiasSupply,
@@ -284,7 +290,7 @@ struct TetherSensors {
 	repeller: Repeller,
 }
 
-#[derive(Debug)]
+#[derive(Debug, serde::Serialize)]
 struct CathodeOffsetSupply {
 	voltage: i32,
 	current: i32,
@@ -292,7 +298,7 @@ struct CathodeOffsetSupply {
 	c_acc: f32,
 }
 
-#[derive(Debug)]
+#[derive(Debug, serde::Serialize)]
 struct TetherBiasSupply {
 	voltage: i32,
 	current: i32,
@@ -300,7 +306,7 @@ struct TetherBiasSupply {
 	c_acc: f32,
 }
 
-#[derive(Debug)]
+#[derive(Debug, serde::Serialize)]
 struct HeaterSupply {
 	voltage: i32,
 	current: i32,
@@ -308,7 +314,7 @@ struct HeaterSupply {
 	c_acc: f32,
 }
 
-#[derive(Debug)]
+#[derive(Debug, serde::Serialize)]
 struct Repeller {
 	voltage: i32,
 	v_acc: f32,
